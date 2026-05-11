a84a87c32df32299bacf21d00cc99ee7be0a655a
Loischsiy <90323392+Loischsiy@users.noreply.github.com>
2026-05-11 22:13:01 +0300

    fix: add missing _service entrypoint and vpn plugin

---
+++ lib/plugins/vpn.dart
@@ -0,0 +1,79 @@
+import 'dart:convert';
+
+import 'package:meow_clash/clash/clash.dart';
+import 'package:meow_clash/models/models.dart';
+import 'package:meow_clash/state.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+
+abstract mixin class VpnListener {
+  void onDnsChanged(String dns) {}
+}
+
+class Vpn {
+  static final Vpn _instance = Vpn._internal();
+  final MethodChannel methodChannel = const MethodChannel('vpn');
+
+  Vpn._internal() {
+    methodChannel.setMethodCallHandler((call) async {
+      switch (call.method) {
+        case 'gc':
+          clashCore.requestGc();
+        case 'closeConnections':
+          clashCore.closeConnections();
+        case 'status':
+          return clashLibHandler?.getRunTime() != null;
+        case 'dnsChanged':
+          for (final listener in _listeners) {
+            listener.onDnsChanged(call.arguments as String);
+          }
+        default:
+      }
+    });
+  }
+
+  factory Vpn() => _instance;
+
+  final ObserverList<VpnListener> _listeners = ObserverList<VpnListener>();
+
+  Future<bool?> start(AndroidVpnOptions options) async {
+    return await methodChannel.invokeMethod<bool>('start', {
+      'data': jsonEncode(options),
+    });
+  }
+
+  Future<bool?> stop() => methodChannel.invokeMethod<bool>('stop');
+
+  Future<List<String>> getLocalIpAddresses() async {
+    return await methodChannel.invokeListMethod<String>(
+          'getLocalIpAddresses',
+        ) ??
+        const [];
+  }
+
+  Future<void> setSmartStopped(bool value) async {
+    await methodChannel.invokeMethod<bool>('setSmartStopped', {'value': value});
+  }
+
+  Future<bool> isSmartStopped() async {
+    return await methodChannel.invokeMethod<bool>('isSmartStopped') ?? false;
+  }
+
+  Future<bool?> smartStop() => methodChannel.invokeMethod<bool>('smartStop');
+
+  Future<bool?> smartResume(AndroidVpnOptions options) async {
+    return await methodChannel.invokeMethod<bool>('smartResume', {
+      'data': jsonEncode(options),
+    });
+  }
+
+  Future<bool> getStatus() async {
+    return await methodChannel.invokeMethod<bool>('status') ?? false;
+  }
+
+  void addListener(VpnListener listener) => _listeners.add(listener);
+
+  void removeListener(VpnListener listener) => _listeners.remove(listener);
+}
+
+Vpn? get vpn => globalState.isService ? Vpn() : null;

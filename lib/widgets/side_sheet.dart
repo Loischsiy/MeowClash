0c9c01bd586714e65b18fa55a22d9b120d8f9be8
Loischsiy <90323392+Loischsiy@users.noreply.github.com>
2026-06-19 22:40:13 +0300

    Remove dead widget builders from codebase
    Remove commented-out widget builders from `ScrollOverBuilder` and
    `showModalSideSheet`

--- lib/widgets/side_sheet.dart
+++ lib/widgets/side_sheet.dart
@@ -590,28 +590,3 @@ Future<T?> showModalSideSheet<T>({
     useSafeArea: useSafeArea,
   ));
 }
-
-// class ModalAppBar extends StatelessWidget {
-//   final String title;
-//
-//   const ModalAppBar({
-//     super.key,
-//     required this.title,
-//   });
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return AppBar(
-//       automaticallyImplyLeading: false,
-//       title: Text(title),
-//       centerTitle: false,
-//       actions: const [
-//         SizedBox(
-//           height: kToolbarHeight,
-//           width: kToolbarHeight,
-//           child: CloseButton(),
-//         )
-//       ],
-//     );
-//   }
-// }
